d8092bca0937c1ac89160b965776f378f5ca33ae
urvas <urvas@LAPTOP-8IL23MLV.home>
2020-02-17 11:23:38 -0500

    bye bye saaasu

--- HelloWorld/src/Ashok/neelam.java
+++ HelloWorld/src/Ashok/neelam.java
@@ -5,13 +5,15 @@ import Brijesh.Brijesh;
 public class neelam {
 	
 	
-	Brijesh P1 = new Brijesh();
-	int P2 = P1.sum(1,2);
+
 
 	public static int Babbal(int a, int b) {
 		Brijesh P1 = new Brijesh();
 		int P2 = P1.sum(1,2);
 		return P2;
 	}
+	
+	Brijesh P1 = new Brijesh();
+	int P2 = P1.sum(1,2);
 	
 }
